35dbe61c89fc771ef8656856f4fcf31950017e84
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-06-02 14:42:11 -0500

    refac: update Button component to include success variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        success: 'bg-green-700 text-white shadow-sm hover:bg-green-800',
       },
       size: {
         default: 'h-9 px-4 py-2',
@@ -38,14 +39,20 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  capitalize?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, size, asChild = false, capitalize = true, ...props },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : 'button';
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }), {
+          capitalize: capitalize,
+        })}
         ref={ref}
         {...props}
       />
