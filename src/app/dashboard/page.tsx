17d0f049a32ee0bb18b5a4dcbd8d71f46a56fd58
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-06-02 15:07:56 -0500

    feat: redirect to '/dashboard/accordion' in dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dashboard
-    </main>
-  );
+  redirect('/dashboard/accordion');
 }
