ac80b5ecbc086deececa2d5555f428b35b0a1dc6
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-06-02 10:47:22 -0500

    feat: add site configuration file with name, description, and navigation links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
+import '@/styles/global.css';
+
 import type { Metadata } from 'next';
 
+import { siteConfig } from '@/config/site';
 import { fontSans } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
 
-import '@styles/global.css';
-
 export const metadata: Metadata = {
-  title: 'ShadcnUI Dashboard',
-  description: 'ShadcnUI Dashboard',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
   icons: {
     icon: '/favicon.ico',
   },
